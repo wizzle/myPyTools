352931e5c3ee120bafc440eda9db571e06680ba8
wizzle <makdorf@gmail.com>
2016-09-05 23:26:56 +0300

    refactor

--- tools/password_generator.py
+++ tools/password_generator.py
@@ -1,49 +1,64 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 import random
 import argparse
 
 
 def get_char_for_password(what):
     """
-    To avoid ambiguous symbols alphabet not full.
-    Full version - abcdefghijklmnopqrstuvwxyz
-    :param what:
-    :return: char
+    To avoid ambiguous symbols alphabet is not full.
+    Full version of alphabet - 'abcdefghijklmnopqrstuvwxyz'.
+
+    :param: int(what)
+    :return: string(char)
+
+    >>> (get_char_for_password(1)).islower()
+    True
+    >>> (get_char_for_password(2)).isupper()
+    True
+    >>> (get_char_for_password(3)).isdigit()
+    True
     """
     alphabet = 'abcdefghjkmnpqrstuvwxyz'
     return {
-        '1': random.choice(alphabet),
-        '2': random.choice(alphabet.upper()),
-        '3': str(random.randrange(1, 9, 1)),
-    }.get(what, '0')
+        1: random.choice(alphabet),
+        2: random.choice(alphabet.upper()),
+        3: str(random.randrange(1, 9, 1)),
+    }.get(what, 0)
 
 
 def get_pass(length):
+    """
+    Function for generate required password.
+
+    :param length:
+    :return: string. len(string) == :param length
+
+    >>> len(get_pass(10)) == 10
+    True
+    >>> (get_pass(10)).isalnum()
+    True
+    """
     pw_len = int(length)
     pw = ''
     for i in range(pw_len):
-        char_type = str(random.randint(1, 3))
+        char_type = random.randint(1, 3)
         pw += get_char_for_password(char_type)
     return pw
 
 
-def get_passwords(count=10, length=10):
-    cnt = int(count)
-    pwlist = []
-    while cnt:
-        pwlist.append(get_pass(length))
-        cnt -= 1
-    return pwlist
-
-
 parser = argparse.ArgumentParser(description='Password generator.')
-parser.add_argument('count_passwords', type=int, help='Count of passwords will be generated.', default=10, nargs='?')
-parser.add_argument('length_one_password', type=int, help='Length of one password.', default=12, nargs='?')
+parser.add_argument('passwords_count', type=int, default=10, help='The number of passwords to be generated. Default 10')
+parser.add_argument('password_length', type=int, default=12, help='The length of the password. Default 12')
 args = parser.parse_args()
-cp = args.count_passwords
+cp = args.passwords_count
 if cp > 100:
     cp = 100
-lop = args.length_one_password
-passwords = get_passwords(cp, lop)
-for password in passwords:
-    print('%d:\t%s' % (passwords.index(password)+1, password))
+pl = args.password_length
+passwords = {i + 1: get_pass(pl) for i in range(cp)}
+for pass_number in passwords.keys():
+    print('%d:\t%s' % (pass_number, passwords[pass_number]))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
